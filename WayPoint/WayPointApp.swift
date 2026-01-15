84c8534d743ad8926ed56ea39da4b58044a41172
王文友 <wangwenyou@hotmail.com>
2026-01-15 17:06:57 +0800

    Initial Commit

---
+++ WayPoint/WayPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayPointApp.swift
+//  WayPoint
+//
+//  Created by 王文友 on 2026/1/15.
+//
+
+import SwiftUI
+
+@main
+struct WayPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
